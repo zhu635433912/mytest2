38a28999c688f9416ce943ecd9374041a05a4956
zhu635433912 <1335112702@qq.com>
2016-02-29 16:58:00 +0800

    修改了测试

--- app/src/main/java/com/zhuyunjian/mytest/MainActivity.java
+++ app/src/main/java/com/zhuyunjian/mytest/MainActivity.java
@@ -2,12 +2,15 @@ package com.zhuyunjian.mytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("测试测试");
     }
 }
